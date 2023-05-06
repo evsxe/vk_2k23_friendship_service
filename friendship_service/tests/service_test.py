7cc70b9bbf3c846bcd1abbd19f05d275b124bbeb
Evgeniy <saluev.evgeniy@gmail.com>
2023-05-06 14:13:04 +0300

    Added template tests for project

---
+++ friendship_service/tests/service_test.py
@@ -0,0 +1,12 @@
+from unittest import TestCase, main
+
+import friendship_service.manage
+
+
+class ServiceTest(TestCase):
+    def template_test(self):
+        pass
+
+
+if __name__ == '__main__':
+    main()
